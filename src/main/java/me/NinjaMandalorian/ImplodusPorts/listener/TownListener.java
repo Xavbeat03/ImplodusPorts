9dbd337f01c8978e529ee7d4610067a1b6f639ea
Xavier Beatrice <Xavbeat03@gmail.com>
2024-01-12 16:21:19 -0500

    More work on getting ports too properly delete themselves and catching possible errors related.

--- src/main/java/me/NinjaMandalorian/ImplodusPorts/listener/TownListener.java
+++ src/main/java/me/NinjaMandalorian/ImplodusPorts/listener/TownListener.java
@@ -1,9 +1,9 @@
 package me.NinjaMandalorian.ImplodusPorts.listener;
 
 import com.palmergames.bukkit.towny.event.town.TownRuinedEvent;
+import com.palmergames.bukkit.towny.event.town.TownUnclaimEvent;
 import me.NinjaMandalorian.ImplodusPorts.ImplodusPorts;
 import me.NinjaMandalorian.ImplodusPorts.Logger;
-import me.NinjaMandalorian.ImplodusPorts.data.PortDataManager;
 import me.NinjaMandalorian.ImplodusPorts.object.Port;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,8 +18,20 @@ public class TownListener implements Listener{
 	@EventHandler
 	public void onTownRuinedEvent(TownRuinedEvent e) {
 		if(ImplodusPorts.getInstance().isTownyEnabled()){
-		Logger.log("Town ruined event, deleting relevant ports.");
+			Logger.log("Town ruined event, deleting relevant ports.");
 			Port.portDestroy(Port.getPort(e.getTown()));
 		}
 	}
+
+	@EventHandler
+	public void onTownUnclaimEvent(TownUnclaimEvent e) {
+		if(ImplodusPorts.getInstance().isTownyEnabled()){
+			Logger.log("Town unclaim event, deleting relevant ports.");
+			Port.portDestroy(Port.getPort(e.getTown()));
+		}
+	}
+
+
+
+
 }
