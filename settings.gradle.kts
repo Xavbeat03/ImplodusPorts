817ceae1fba7844e453ba95992f19f4df7c85e7a
Xavier Beatrice <Xavbeat03@gmail.com>
2024-09-22 02:25:26 -0400

    build: Server runs, but issue with unsupported API version on plugin

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" // allow automatic download of JDKs 
+}
+
+rootProject.name = "ImplodusPorts"
