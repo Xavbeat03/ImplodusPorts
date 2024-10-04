4dd09cdbfa54bed3adf4494d1917cc652a6ca7b8
Xavier Beatrice <58835993+Xavbeat03@users.noreply.github.com>
2024-10-04 19:46:42 -0400

    fixing changesize CommandChangeSize

--- src/main/java/me/ninjamandalorian/implodusports/command/CommandChangeSize.java
+++ src/main/java/me/ninjamandalorian/implodusports/command/CommandChangeSize.java
@@ -4,11 +4,15 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.StringArgument;
 import me.ninjamandalorian.implodusports.handler.TravelHandler;
+import me.ninjamandalorian.implodusports.helper.PortHelper;
 import me.ninjamandalorian.implodusports.helper.StringHelper;
 import me.ninjamandalorian.implodusports.object.Port;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class CommandChangeSize {
 	
@@ -22,20 +26,31 @@ public class CommandChangeSize {
 			.withShortDescription("Changes the size of the player")
 			.withArguments((new StringArgument("size")).replaceSuggestions(intSuggestion))
 			.executesPlayer((player, args) -> {
-				travelCommand(player, StringHelper.remFirst(args.rawArgs()));
+				changeSizeCommand(player, StringHelper.remFirst(args.rawArgs()));
 			});
 	}
 
-	private static void travelCommand(Player player, String[] args) {
-		//Bukkit.getLogger().info("Travel recieved with destinations: " + args.toString());
 
-		@SuppressWarnings("unused")
-		String[] extraParams = Arrays.copyOfRange(args, 2, args.length - 1);
-		Port origin = Port.getPort(args[0]);
-		Port destination = Port.getPort(args[1]);
-		if (origin != null && destination != null) {
-			TravelHandler.startJourney(player, origin, destination, args);
+	private static void changeSizeCommand(Player player, String[] args) {
+		int newSize = 0;
+		try {
+			newSize = Math.max(Math.min(4, Integer.parseInt(args[0])), 1);
+		} catch (NumberFormatException e) {
+			return;
 		}
+		Block targetBlock = player.getTargetBlock(null, 5);
+		if (targetBlock == null) return;
+		Port port = Port.getPort(targetBlock);
+		if (port == null) return;
+		port.changeSize(newSize);
+
+		Sign sign = (Sign) targetBlock.getState();
+		List<String> signList = PortHelper.formatSign(port);
+		for (int i = 0; i < 4; i++) {
+			sign.setLine(i, signList.get(i));
+		}
+		sign.update(true);
+
 		return;
 	}
 }
